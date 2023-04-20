fd0877120e1707d16e4f82fef9266ec61f08afc1
Mariya <mariyabaig0106@gmail.com>
2023-04-20 14:47:57 +0530

    style: Updated navbar design with icons

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,20 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import dashboardIcon from "../assets/business-report.png";
+import expensesIcon from "../assets/expenses.png";
+import incomeIcon from "../assets/income.png";
 
 const Navbar = () => {
-
   return (
     <div className="card ">
       <div className="card-overlay"></div>
-      <ul className="flex flex-row justify-between items-center ">
-        <li className=" flex flex-col items-center">
-          <Link to="/">Dashboard</Link>
+      <ul className="flex flex-row justify-between ">
+        <li>
+          <Link to="/">
+            <img src={dashboardIcon} alt="Dashboard Icon" />
+            Dashboard
+          </Link>
         </li>
-        <li className=" flex flex-col items-center ">
-          <Link to="/income">Income</Link>
+        <li>
+          <Link to="/income">
+            <img src={incomeIcon} alt="Income Icon" />
+            Income
+          </Link>
         </li>
-        <li className=" flex flex-col items-center ">
-          <Link to="/expenses">Expenses</Link>
+        <li>
+          <Link to="/expenses">
+            <img src={expensesIcon} alt="Expenses Icon" />
+            Expenses
+          </Link>
         </li>
       </ul>
     </div>
