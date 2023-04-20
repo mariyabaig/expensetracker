cbb4157ffa732ca881a16b760c647db06c20e601
Mariya <mariyabaig0106@gmail.com>
2023-04-20 15:40:52 +0530

    fix: Refactored to show all submitted expenses
    instead of only the latest submitted expense

--- frontend/src/pages/Expenses.js
+++ frontend/src/pages/Expenses.js
@@ -1,34 +1,39 @@
 import React, { useState } from "react";
 
 const Expenses = () => {
+  // Set up state for the form inputs and submitted data
   const [expense, setExpense] = useState({
     amount: "",
     date: "",
     category: "",
   });
-  const [submittedData, setSubmittedData] = useState(null)
+  // Set up an array to store submitted data
+  const [submittedData, setSubmittedData] = useState([]);
 
+  // Handle form submission
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(setExpense);
-    const data={
-      amount:expense.amount,
-      date:expense.date,
-      category:expense.category
-    }
-    setSubmittedData(data)
+    // Create an object with the form data
+    const data = {
+      amount: expense.amount,
+      date: expense.date,
+      category: expense.category,
+    };
+    // Add the object to the submittedData array
+    setSubmittedData([...submittedData, data]);
   };
+
+  // This state variable is used to store the submitted data from the form
+  //  It is initialized to null because no data has been submitted yet
+  // const [submittedData, setSubmittedData] = useState(null);
 
   return (
     <>
-    <div>
-    <div className="card">
-      <div className="card-overlay"></div>
-      <form
-            onSubmit={handleSubmit}
-
-          >
-            <label >
+      <div>
+        <div className="card">
+          <div className="card-overlay"></div>
+          <form onSubmit={handleSubmit}>
+            <label>
               Expense Amount
               <input
                 type="number"
@@ -39,7 +44,7 @@ const Expenses = () => {
                 required
               />
             </label>
-            <label >
+            <label>
               Date
               <input
                 type="date"
@@ -50,7 +55,7 @@ const Expenses = () => {
                 required
               />
             </label>
-            <label >
+            <label>
               Category
               <select
                 value={expense.category}
@@ -58,7 +63,6 @@ const Expenses = () => {
                   setExpense({ ...expense, category: event.target.value })
                 }
                 required
-                
               >
                 <option value="">--Select a category--</option>
                 <option value="Food">Food</option>
@@ -69,19 +73,39 @@ const Expenses = () => {
               </select>
             </label>
             <button className="bg-blue-300 mt-4" type="submit">
-             Submit
+              Submit
             </button>
           </form>
+        </div>
+        {/* Display submitted data if there is any */}
+        {submittedData.length > 0 && (
+          <div>
+            {/* This block of code is conditional rendering
+ It only renders if submittedData is not null (meaning data has been submitted)
+ It displays the submitted data in a card format
+ The submitted data is accessed from the submittedData state variable
+ Each piece of data (amount, date, category) is displayed in a separate <p> element */}
+            {/* {submittedData && (
+          <div className="card">
+            <div className="card-overlay"></div>
+            <h2>Submitted Data</h2>
+            <p>Amount: {submittedData.amount}</p>
+            <p>Date: {submittedData.date}</p>
+            <p>Category: {submittedData.category}</p>
           </div>
-          {submittedData && (
-              <div>
-                <h2>Submitted Data</h2>
-                <p>Amount: {submittedData.amount}</p>
-                <p>Date: {submittedData.date}</p>
-                <p>Category: {submittedData.category}</p>
+        )} */}
+            <h2>Submitted Data</h2>
+            {/* Map over the submittedData array and display each object */}
+            {submittedData.map((data, index) => (
+              <div key={index}>
+                <p>Amount: {data.amount}</p>
+                <p>Date: {data.date}</p>
+                <p>Category: {data.category}</p>
               </div>
-            )}
+            ))}
           </div>
+        )}
+      </div>
     </>
   );
 };
