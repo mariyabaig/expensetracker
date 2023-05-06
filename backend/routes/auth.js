a7945ecfa00f353b17efedbac7f6a40f72ff4af9
Mariya <mariyabaig0106@gmail.com>
2023-05-06 12:42:45 +0530

    feat: endpoints for login and signup added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,27 +1,106 @@
 const express = require('express');
-const User = require('../models/User');
-const router = express.Router(); 
+const User = require('../models/userSchema');
+const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+require('dotenv').config()
 
+const JWT_SECRET = process.env.JWT_SECRET;
 
-// Create a User using: POST "/api/auth/". Doesn't require Auth
-router.post('/',[
-    body('name', 'Enter a valid name').isLength({ min: 3 }),
-    body('email', 'Enter a valid email').isEmail(),
-    body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
-] , (req, res)=>{ 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+// ROUTE 1: Create a User using: POST "/register". No login required
+router.post('/signup', [
+  body('name', 'Enter a valid name').isLength({ min: 3 }),
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
+], async (req, res) => {
+  let success = false;
+  // If there are errors, return Bad request and the errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) { 
+    return res.status(400).json({success, errors: errors.array() });
+  }
+  try {
+    // Check whether the user with this email exists already
+    let user = await User.findOne({success, email: req.body.email });
+    if (user) {
+      return res.status(400).json({success, error: "Sorry a user with this email already exists" })
     }
-    User.create({
-        name: req.body.name,
-        password: req.body.password,
-        email: req.body.email,
-      }).then(user => res.json(user))
-      .catch(err=> {console.log(err)
-    res.json({error: 'Please enter a unique value for email', message: err.message})})
- 
-} )
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt);
 
+    // Create a new user
+    user = await User.create({
+      name: req.body.name,
+      password: secPass,
+      email: req.body.email,
+    });
+    const data = {
+      user: {
+        id: user.id
+      }
+    }
+    const authtoken = jwt.sign(data, JWT_SECRET);
+
+
+
+    success= true
+    res.json({success, authtoken ,  userId: user.id})
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+})
+
+
+// ROUTE 2: Authenticate a User using: POST "/login". No login required
+router.post('/login', [
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Password cannot be blank').exists(),
+], async (req, res) => {
+  let success = false;
+  // If there are errors, return Bad request and the errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
+      success = false
+      return res.status(401).json({ error: "Please try to login with correct credentials" });
+    }
+
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+      success = false
+      return res.status(401).json({ success, error: "Please try to login with correct credentials" });
+    }
+
+    const data = {
+      user: {
+        id: user.id
+      }
+    }
+    const authtoken = jwt.sign(data, JWT_SECRET);
+    success = true;
+    res.json({ success, authtoken })
+    res.json({ token, userId: user._id });
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+
+
+});
+
+
+
+  
+  
+  
 module.exports = router
