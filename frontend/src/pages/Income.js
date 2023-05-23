84ea59008818a52d76034e1a0549e0c071514a7a
Mariya <mariyabaig0106@gmail.com>
2023-05-23 12:18:10 +0530

    feat: added present day's income's card

--- frontend/src/pages/Income.js
+++ frontend/src/pages/Income.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { DateTime } from "luxon";
 
 const Income = () => {
@@ -103,6 +103,25 @@ const Income = () => {
     setSelectedMonth((prevMonth) => (prevMonth === month ? null : month));
   };
 
+  const todaysData = (data) => {
+    const today = DateTime.local().toISODate(); // Get today's date in ISO format
+    return data.reduce((results, curr) => {
+      const currDate = DateTime.fromISO(curr.date).toISODate(); // Convert income date to ISO format
+      if (currDate === today) {
+        // Compare income date with today's date
+        if (!results[today]) {
+          results[today] = { data: [], total: 0 };
+        }
+        results[today].data.push(curr); // Push current data item to today's data array
+        results[today].total += parseInt(curr.amount); // Add current data item's amount to today's total
+      }
+      return results;
+    }, {});
+  };
+
+  const today = DateTime.local().toISODate();
+  const todaysIncome = todaysData(submittedData)[today]; // Call todaysData function to get today's data
+
   return (
     <>
       <div className="flex flex-row">
@@ -155,34 +174,68 @@ const Income = () => {
         <div className="my-5 mx-5">
           {submittedData.length > 0 ? (
             <>
+              <div className="flex">
+                <div className="flex flex-row ">
+                  {todaysIncome && todaysIncome.total && (
+                    <div className="card my-3 text-center">
+                      <div className="card-overlay"></div>
+                      <h3 className="text-md text-center font-bold">
+                        {DateTime.local().toFormat("dd LLL yyyy")}'s Incomes
+                      </h3>
+                      <p>Total: ${todaysIncome.total}</p>
+                      <table>
+                        <thead>
+                          <tr className="">
+                            <th>Category</th>
+                            <th>Amount</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {todaysIncome.data.map((income, index) => (
+                            <tr key={index}>
+                              <td>{income.category}</td>
+                              <td>${income.amount}</td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+                    </div>
+                  )}
+
+                  {(!todaysIncome || !todaysIncome.total) && (
+                    <div>
+                      <h3>Today's Income</h3>
+                      <p>No data for today</p>
+                    </div>
+                  )}
+                </div>
+              </div>
               {Object.entries(groupedData).map(([month, data]) => (
                 <div className="card" key={month}>
                   <div className="card-overlay"></div>
-                  <h2 onClick={()=>handleMonthClick(month)} className="font-bold">{month}'s total : {data.total}</h2>
+                  <h2 onClick={() => handleMonthClick(month)} className="font-bold">
+                    {month}'s total: {data.total}
+                  </h2>
 
                   {selectedMonth === month && (
-                  <table>
-                    <tbody>
-                      {data.data.map((item, index) => (
-                        <tr key={index}>
-                          <td>{item.amount}</td>
-                          <td>{item.category}</td>
-                          <td>
-                            {DateTime.fromISO(item.date).toFormat("dd LLL yy")}
-                          </td>
-                          <td>
-                            <button onClick={() => handleEdit(index)}>
-                              Edit
-                            </button>
-                            <button onClick={() => handleDelete(index)}>
-                              Delete
-                            </button>
-                          </td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table> )}
-  
+                    <table>
+                      <tbody>
+                        {data.data.map((item, index) => (
+                          <tr key={index}>
+                            <td>{item.amount}</td>
+                            <td>{item.category}</td>
+                            <td>
+                              {DateTime.fromISO(item.date).toFormat("dd LLL yy")}
+                            </td>
+                            <td>
+                              <button onClick={() => handleEdit(index)}>Edit</button>
+                              <button onClick={() => handleDelete(index)}>Delete</button>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  )}
                 </div>
               ))}
             </>
