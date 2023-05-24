4d2ae38228caf20caf8a2041ec6cd57a548cac97
Mariya <mariyabaig0106@gmail.com>
2023-05-24 21:30:49 +0530

    feat: logged in user's name dispalyed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,18 +6,54 @@ import Income from './pages/Income';
 import './App.css';
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 function App() { 
   const [isLoggedin, setIsLoggedin] = useState(false);
+  const [userName, setUserName] = useState("")
+
   const handleLogout = () => {
     localStorage.clear();
-    setIsLoggedin(false);
-    
-  }
+    setIsLoggedin(false);}
+    // Fetch user details when the component mounts
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      try {
+        const response = await fetch("http://localhost:8000/getuser", {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+            authtoken: localStorage.getItem("authtoken"),
+          },
+        });
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setUserName(data.name);
+        console.log(userName)
+      } catch (err) {
+        console.error(err);
+        // Handle error
+      }
+    };
+
+    // Fetch user details only if the user is logged in
+    if (isLoggedin) {
+      fetchUserDetails();
+      
+    }
+  }, [isLoggedin]);
+
+  
   return (
+  
     <BrowserRouter>
+     
       <Navbar isLoggedin={isLoggedin} handleLogout={handleLogout} />
+      {isLoggedin ? ( <h1 className="text-light-gray text-2xl"> Hey, {userName}! </h1>) : null}
+     
       <Routes>
         <Route
           exact
