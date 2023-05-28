27c2dba39469040629f0729502f7880f724a606b
Mariya <mariyabaig0106@gmail.com>
2023-05-29 01:53:12 +0530

    style: removed floating labels

--- frontend/src/pages/Expenses.js
+++ frontend/src/pages/Expenses.js
@@ -109,94 +109,82 @@ const Expenses = () => {
   return (
     <>
       <div className=" py-6 sm:py-12 x">
-        <form onSubmit={handleSubmit}>
-          <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-            <div className="absolute inset-0 bg-gradient-to-r from-green to-green shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-xl"></div>
-            <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-xl sm:p-12">
-              <div className="max-w-md mx-auto">
-                <div>
-                  <h1 className="text-2xl font-semibold">
-                    Add your expenses now
-                  </h1>
-                </div>
-                <div className="divide-y divide-gray-200">
-                  <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                    <div className="relative">
-                      <input
-                        autocomplete="off"
-                        value={expense.amount}
-                        id="amount"
-                        name="amount"
-                        type="number"
-                        className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
-                        placeholder="Expense amount"
-                        onChange={(event) =>
-                          setExpense({
-                            ...expense,
-                            amount: event.target.value,
-                          })
-                        }
-                      />
-                      <label
-                        htmlFor="email"
-                        className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
-                      >
-                        Expense Amount
-                      </label>
-                    </div>
+      <form onSubmit={handleSubmit}>
+  <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+    <div className="absolute inset-0 bg-gradient-to-r from-green to-green shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-xl"></div>
+    <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-xl sm:p-12">
+      <div className="max-w-md mx-auto">
+        <div>
+          <h1 className="text-2xl font-semibold">
+            Add your expenses now
+          </h1>
+        </div>
+        <div className="divide-y divide-gray-200">
+          <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+            <div className="relative">
+              <input
+                autoComplete="off"
+                value={expense.amount}
+                id="amount"
+                name="amount"
+                type="number"
+                className="h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600"
+                placeholder="Expense amount"
+                onChange={(event) =>
+                  setExpense({
+                    ...expense,
+                    amount: event.target.value,
+                  })
+                }
+              />
+            </div>
 
-                    <div className="relative">
-                      <input
-                        autocomplete="off"
-                        type="date"
-                        value={expense.date}
-                        onChange={(event) =>
-                          setExpense({ ...expense, date: event.target.value })
-                        }
-                        required
-                        id="date"
-                        name="date"
-                        className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
-                        placeholder="Date"
-                      />
-                      <label
-                        htmlFor="date"
-                        className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
-                      ></label>
-                    </div>
+            <div className="relative">
+              <input
+                autoComplete="off"
+                type="date"
+                value={expense.date}
+                onChange={(event) =>
+                  setExpense({ ...expense, date: event.target.value })
+                }
+                required
+                id="date"
+                name="date"
+                className="h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600"
+                placeholder="Date"
+              />
+            </div>
 
-                    <div className="relative">
-                      <label>
-                        <select
-                          value={expense.category}
-                          onChange={(event) =>
-                            setExpense({
-                              ...expense,
-                              category: event.target.value,
-                            })
-                          }
-                          required
-                        >
-                          <option value="">Expense Category</option>
-                          <option value="Food">Food</option>
-                          <option value="Transportation">Transportation</option>
-                          <option value="Housing">Housing</option>
-                          <option value="Entertainment">Entertainment</option>
-                          <option value="Utilities">Utilities</option>
-                        </select>
-                      </label>
-                    </div>
-                    <div className="relative">
-                      <button className="bg-dark-blue text-light-gray rounded-md px-2 py-1">
-                        Submit
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
+            <div className="relative">
+              <select
+                value={expense.category}
+                onChange={(event) =>
+                  setExpense({ ...expense, category: event.target.value })
+                }
+                required
+                className="h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600"
+              >
+                <option value="">Expense Category</option>
+                <option value="Food">Food</option>
+                <option value="Transportation">Transportation</option>
+                <option value="Housing">Housing</option>
+                <option value="Entertainment">Entertainment</option>
+                <option value="Utilities">Utilities</option>
+              </select>
+            </div>
+
+            <div className="relative">
+              <button className="bg-dark-blue text-light-gray rounded-md px-2 py-1">
+                Submit
+              </button>
             </div>
           </div>
-        </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</form>
+
       </div>
       <div className="h-screen">
         {submittedData.length > 0 ? (
