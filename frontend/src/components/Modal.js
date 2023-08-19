c08e6c05d5cec0727400bba64be9fe3796fd34a9
Mariya <mariyabaig0106@gmail.com>
2023-08-19 22:37:28 +0530

    Modal created to show alll results

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,12 +1,81 @@
 import React from "react";
+import { DateTime } from "luxon";
+import { Pie } from "react-chartjs-2";
 
-const Modal = () => {
+const Modal = ({ isOpen, onClose, data }) => {
+  // Calculate data for the Pie chart
+  const categoryAmounts = data.reduce((acc, expense) => {
+    if (!acc[expense.category]) {
+      acc[expense.category] = 0;
+    }
+    acc[expense.category] += parseFloat(expense.amount);
+    return acc;
+  }, {});
+
+  const pieChartData = {
+    labels: Object.keys(categoryAmounts),
+    datasets: [
+      {
+        data: Object.values(categoryAmounts),
+        backgroundColor: [
+          "#FF6384",
+          "#36A2EB",
+          "#FFCE56",
+          // Add more colors if needed
+        ],
+      },
+    ],
+  };
+
   return (
-    <>
-      
-
-      
-    </>
+    <div
+      className={`fixed inset-0 flex items-center justify-center transition-opacity  ${
+        isOpen ? "opacity-100" : "opacity-0 pointer-events-none"
+      }`}
+    >
+      <div className="absolute inset-0 backdrop-filter backdrop-blur-3xl bg-black opacity-60"></div>
+      <div className="bg-white p-4 rounded-lg w-90 max-w-screen-md relative">
+        <h2 className="text-lg font-semibold mb-4">All Entries</h2>
+        <div className=" flex items-center justify-center">
+        {/* First Row - Table */}
+        <div className="mb-4" >
+          <table className="table-auto w-full">
+            <thead className="text-xs font-semibold uppercase text-dark-blue">
+              <tr>
+                <th className="py-2 px-2 whitespace-nowrap">Amount</th>
+                <th className="py-2 px-2 whitespace-nowrap">Category</th>
+                <th className="py-2 px-2 whitespace-nowrap">Date</th>
+              </tr>
+            </thead>
+            <tbody className="text-sm">
+              {data.map((expense, index) => (
+                <tr key={index}>
+                  <td className="py-2 px-2 whitespace-nowrap">{expense.amount}</td>
+                  <td className="py-2 px-2 whitespace-nowrap">{expense.category}</td>
+                  <td className="py-2 px-2 whitespace-nowrap">
+                    {DateTime.fromISO(expense.date).toFormat("dd LLL yyyy")}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        
+        {/* Second Row - Pie Chart */}
+        <div className="mt-4">
+          <Pie data={pieChartData} />
+        </div>
+        </div>
+        <div>
+          <button
+            className="mt-4 bg-dark-blue text-light-gray rounded-md px-2 py-1"
+            onClick={onClose}
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
