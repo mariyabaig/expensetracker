bb2a5e4cffab9b8e36ef8e7c91d265ec7876093e
Mariya <mariyabaig0106@gmail.com>
2023-08-30 13:48:43 +0530

    icons added

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link ,useNavigate} from "react-router-dom";
-// import dashboardIcon from "../assets/business-report.png";
-// import expensesIcon from "../assets/expenses.png";
-// import shutdown from "../assets/shutdown.png";
-// import incomeIcon from "../assets/income.png";
+import dashboardIcon from "../assets/business-report.png";
+import expensesIcon from "../assets/expenses.png";
+import shutdown from "../assets/shutdown.png";
+import incomeIcon from "../assets/income.png";
 
 const Navbar = ({setIsLoggedin,handleLogout}) => {
     const navigate = useNavigate();
@@ -21,25 +21,25 @@ const Navbar = ({setIsLoggedin,handleLogout}) => {
       <ul className="flex flex-row h-16 justify-evenly items-center bg-dark-blue text-light-gray text-md ">
         <li >
           <Link to="/">
-            {/* <img src={dashboardIcon} alt="Dashboard Icon" /> */}
+         <img src={dashboardIcon} alt="Dashboard Icon" /> 
             Dashboard
           </Link>
         </li>
         <li>
           <Link to="/income">
-            {/* <img src={incomeIcon} alt="Income Icon" /> */}
+            <img src={incomeIcon} alt="Income Icon" /> 
             Income
           </Link>
         </li>
         <li>
           <Link to="/expenses">
-            {/* <img src={expensesIcon} alt="Expenses Icon" /> */}
+          <img src={expensesIcon} alt="Expenses Icon" /> 
             Expenses
           </Link>
         </li>
         <li>
           <button onClick={handleLogout}>
-            {/* <img src={shutdown} alt="Expenses Icon" /> */}
+         <img src={shutdown} alt="Expenses Icon" /> 
             Logout
           </button>
         </li>
